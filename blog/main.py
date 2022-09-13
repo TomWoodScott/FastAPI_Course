ce5387c014abbe564ca042e68c0a510bb9ef6736
Thomas <thomaswoodland-scott@hotmail.com>
2022-09-13 10:51:56 +0100

    create schemas.py

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,9 @@
-import re
-from pydantic import BaseModel
 from fastapi import FastAPI
+from . import schemas
 
 app = FastAPI()
 
-class Blog(BaseModel):
-    title:str
-    body:str
 
 @app.post('/blog')
-def create(request:Blog):
+def create(request:schemas.Blog):
     return {'title':request.title, 'body':request.body}
